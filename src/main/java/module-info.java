b969eb374cd8c842eb2c5f056e43bab16456dfd5
the3face <138581774+the3face@users.noreply.github.com>
2025-12-07 23:33:07 +0100

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.net.http;
-    requires java.xml;
-
-    opens org.example to javafx.fxml;
-    exports org.example;
-}
